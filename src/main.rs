95908425dab5357f8baae49f63f485ce1ad3151f
Zeno <zeno@sevencubed.tech>
2022-08-25 01:41:46 -0400

    Wait for ASCII 32 byte to start

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@
 
 use core::fmt::Write;
 
+#[repr(u8)]
+enum Ascii {
+    Space = 32,
+}
+
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
@@ -13,16 +18,37 @@ unsafe fn main() -> ! {
     let mut periphs = bsp::Peripherals::take().unwrap();
     let pins = bsp::pins::t41::from_pads(periphs.iomuxc);
     let mut led = bsp::configure_led(pins.p13);
-    led.set();
-
     let mut systick = systick::new(cortex_m::Peripherals::take().unwrap().SYST);
-    let (_, mut writer) = usb::split().unwrap();
+
+    let (mut reader, mut writer) = usb::split().unwrap();
     systick.delay_ms(1_000);
     periphs.ccm.pll1.set_arm_clock(
         bsp::hal::ccm::PLL1::ARM_HZ,
         &mut periphs.ccm.handle,
         &mut periphs.dcdc,
     );
+
+    let mut x = 0u8;
+    while x < 3 {
+        use embedded_hal::digital::v2::OutputPin;
+
+        led.set_high().unwrap();
+        systick.delay_ms(30);
+        led.set_low().unwrap();
+        systick.delay_ms(30);
+        x += 1;
+    }
+
+    let mut usb_in = [0; 256];
+    loop {
+        if let Ok(n) = reader.read(&mut usb_in) {
+            if n == 1 && usb_in[0] == Ascii::Space as u8 {
+                break;
+            }
+        } else {
+            panic!();
+        }
+    }
 
     let mut counter = 0u8;
     loop {
